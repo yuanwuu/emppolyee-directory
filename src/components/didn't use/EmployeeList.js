39b320efd4a57d3c097e419350f68610e24db9d0
Yuan Wu <wu.yuanf@gmail.com>
2023-05-15 01:46:41 -0400

    reorganized file structure

---
+++ src/components/didn't use/EmployeeList.js
@@ -0,0 +1,18 @@
+import employeeData from "../../data";
+import { Link } from 'react-router-dom'
+import { useState } from "react";
+
+
+
+export default function EmployeeList(employees) {
+  // const [employees,setEmployees] = useState(employeeData)
+
+  return(
+
+    <>
+    
+    im a employee testing
+    </>
+    
+  )
+}
